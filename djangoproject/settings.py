8a541fa551ffb5f8edbe9b1631ff39729d88fe00
vijaykumarjuwai <vijaykumar@juwai.com>
2017-07-24 15:54:29 +0000

    Change settings

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vijaykumarwebsite',
+	'USER': 'root',
+	'PASSWORD': 'er7mnyxo',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
@@ -119,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+
