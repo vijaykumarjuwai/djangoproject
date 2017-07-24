455f08a7d627137507f6de0aa07ebe4bb18e50f0
vijaykumarjuwai <vijaykumar@juwai.com>
2017-07-25 01:19:41 +0800

    Changed settings

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vijaykumarwebsite',
 	    'USER': 'root',
-	    'PASSWORD': 'tp020654',
+	    'PASSWORD': 'er7mnyxo22',
 	    'HOST': 'localhost',
 	    'PORT': '',
     }
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "website/static"),
