02e48c29957f79b158dfc937183a90c29df20a84
vijaykumarjuwai <vijaykumar@juwai.com>
2017-07-25 18:58:36 +0800

    Added articles model

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Articles
 
 # Register your models here.
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'publish')
+    list_filter = ('status', 'created', 'updated')
+    search_fields = ['title']
+
+
+admin.site.register(Articles, ArticlesAdmin)
