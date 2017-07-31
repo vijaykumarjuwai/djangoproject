18f5d3e91329648db047e9fa6b3d93160847c0f8
vijaykumarjuwai <vijaykumar@juwai.com>
2017-07-31 08:56:19 +0800

    Added detail Page

--- website/views.py
+++ website/views.py
@@ -8,12 +8,14 @@ class ArticleListView(ListView):
     queryset = Article.published.all()
     length = len(queryset)
     context_object_name = 'articles'
-    paginate_by = 10
+    paginate_num = 3
+    paginate_by = paginate_num
     template_name = 'website/index.html'
 
     def get_context_data(self, **kwargs):
         context = super(ArticleListView, self).get_context_data(**kwargs)
         context['length'] = self.length
+        context['paginate_num'] = self.paginate_num
         return context
 
 
